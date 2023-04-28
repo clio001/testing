b6662961bf5b53576cddd8e2c5a889e12972fbf2
clio001 <johnwoitkowitz@hotmail.com>
2023-04-28 19:43:01 +0200

    small edits

--- keyword-retrieval.py
+++ keyword-retrieval.py
@@ -4,7 +4,7 @@ import requests
 
 #Build URL
 baseURL = 'https://sru.gbv.de/opac-de-1?version=1.1&operation=searchRetrieve&maximumRecords=100&recordSchema=picaxml&query=pica.xppn%3D'
-ppn = "798833505"
+ppn = "623903229"
 url = baseURL + ppn
 global data
 
@@ -16,22 +16,18 @@ def getData(url):
     print(f"PPN: {ppn}")
     parseXML(data)
     
-
 #Parse XML
 def parseXML(data):
     root = ET.fromstring(data)
 
     for child in root[2][0][2][0]:
         if child.attrib['tag'] == "021A":
-            print(f"Title: {child[0].text}")
+            print(f"Titel: {child[0].text}")
         
-            
         if child.attrib['tag'] == "028A":
             for e in child:
                 if e.attrib['code'] == "A":
-                    print(f"Author: {e.text}")
-            else:
-                print("Author: 028A does not exist")
+                    print(f"Autor*in: {e.text}")
                         
         if child.attrib['tag'] == "044A":
             for e in child:
