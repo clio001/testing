c94fce0ab26607d4307c9270f0f9af9e41676635
clio001 <johnwoitkowitz@hotmail.com>
2023-04-28 16:15:07 +0200

    Added requests library, added XML parser and files

--- keyword-retrieval.py
+++ keyword-retrieval.py
@@ -1,1 +1,14 @@
-print("Hello")
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('opac-de-1.xml')
+root = tree.getroot()
+
+for child in root[2][0][2][0]:
+    if child.attrib['tag'] == "021A":
+        print(f"Title: {child[0].text}")
+    if child.attrib['tag'] == "028A":
+        for e in child:
+            if e.attrib['code'] == "D":
+                print(f"Author: {e.text}")
+            
+
