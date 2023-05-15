f57ce79e4242883d6b501fa19feb43c39761d02e
clio001 <johnwoitkowitz@hotmail.com>
2023-05-15 10:55:57 +0200

    ppn collection check working

---
+++ collection-ppn-check.py
@@ -0,0 +1,36 @@
+#This file retrieves keywords (Schlagwoerter) from the K10plus library catalogue
+
+import requests, csv, xml.etree.ElementTree as ET
+
+global ppnList
+ppnList = []
+
+def importData():
+    f = open('test-ppns.txt')
+    lines = f.readlines()
+    for line in lines:
+        ppnList.append(line.split('\n')[0])
+    print(ppnList)
+    getData(ppnList)
+
+def getData(ppnList):
+    for ppn in ppnList:
+        url = f"http://sru.k10plus.de/gvk7?version=1.1&operation=searchRetrieve&query=pica.ppn={ppn}&maximumRecords=300&recordSchema=picaxml"
+        response = requests.get(url)
+        data = response.content.decode('utf-8')
+        parseData(data, ppn)
+
+
+def parseData(data,ppn):
+    root = ET.fromstring(data)
+    record = root[2][0][2][0]
+    
+    for datafield in record:    
+        if datafield.attrib['tag'] == "244Z":
+            for subfield in datafield:
+                if subfield.attrib['code'] == "a":
+                    if subfield.text == "Digitale Sammlung Deutscher Kolonialismus":
+                        print(f"Match: PPN{ppn} in DSDK (244Z) gefunden")
+
+
+importData()
